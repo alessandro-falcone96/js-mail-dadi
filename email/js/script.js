4ebeb187eab289efe451fc396d8aa9e58d7e8494
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-01 19:47:01 +0100

    Aggiunto esercizio email utilizzando ciclo for

--- email/js/script.js
+++ email/js/script.js
@@ -8,8 +8,24 @@ var emailUtente = prompt("Inserisci la tua email: ");
 
 document.getElementById('email').innerHTML = emailUtente;
 
-if (listeEmail.includes(emailUtente)) {
+var result = false;
+
+for (var i = 0; i <= 2; i++) {
+  if (emailUtente == listeEmail[i]) {
+    result = true;
+  }
+}
+
+
+if (result == true) {
   document.getElementById('risultato').innerHTML = "Puoi entrare, sei in lista";
-} else{
+} else {
   document.getElementById('risultato').innerHTML = "Mi dispiace, non sei in lista";
 }
+
+
+// if (listeEmail.includes(emailUtente)) {
+//   document.getElementById('risultato').innerHTML = "Puoi entrare, sei in lista";
+// } else{
+//   document.getElementById('risultato').innerHTML = "Mi dispiace, non sei in lista";
+// }
